a0a053a87f9d78b28cc72b66021a93bd6f72525d
Альберт <albiqus@bk.ru>
2022-06-04 19:04:23 +0300

    Редактировали код, сделали функцию checkLength более компактной, устранили некоторые ошибки по Eslint

--- js/main.js
+++ js/main.js
@@ -2,18 +2,12 @@ const getRandomNumber = (startNumber, lastNumber) => {
     if (lastNumber > startNumber) {
         return Math.floor(Math.random() * ((lastNumber + 1) - startNumber)) + startNumber;
     }
-    return console.log('Диапазон введён некорректно');
-}
-
+    return 'Диапазон введён некорректно';
+};
 getRandomNumber(0, 10);
 
 
-
-const isNeedfulLength = (currentLength, maxLength) => {
-    if (currentLength <= maxLength) {
-        return true;
-}
-    return false;
-}
-
-isNeedfulLength(10, 20);
+const checkLength = (text, maxLength) => {
+return text.length <= maxLength;
+};
+checkLength('какой-то текст', 20);
