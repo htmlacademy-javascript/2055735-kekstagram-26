64e5070922f4c2765c4785a3de946df61a686e66
Keks <keks@htmlacademy.ru>
2022-06-05 10:09:08 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,13 +1,11 @@
-const getRandomNumber = (startNumber, lastNumber) => {
-    if (lastNumber > startNumber) {
-        return Math.floor(Math.random() * ((lastNumber + 1) - startNumber)) + startNumber;
-    }
-    return 'Диапазон введён некорректно';
+const getRandomNumber = (startNumber, lastNumber) =>
+{if (lastNumber > startNumber)
+{return Math.floor(Math.random() * ((lastNumber + 1) - startNumber)) + startNumber;}
+return 'Диапазон введён некорректно';
 };
 getRandomNumber(0, 10);
 
 
-const checkLength = (text, maxLength) => {
-return text.length <= maxLength;
-};
+const checkLength = (text, maxLength) =>
+  text.length <= maxLength;
 checkLength('какой-то текст', 20);
