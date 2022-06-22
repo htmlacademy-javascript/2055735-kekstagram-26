a214e4500c19828bf9f373b4bf56a2b4de77663b
Альберт <albiqus@bk.ru>
2022-06-22 21:47:18 +0300

    исправили недочёты

--- js/full-size-image.js
+++ js/full-size-image.js
@@ -1,7 +1,3 @@
-import {getPictures} from './render-pictures.js';
-import {getDescriptions} from './data.js';
-getPictures();
-
 const body = document.querySelector('body');
 const commentCounter = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
@@ -10,45 +6,60 @@ const viewPhoto = document.querySelector('.big-picture__img');
 const likesCount = document.querySelector('.likes-count');
 const commentsCount = document.querySelector('.comments-count');
 const socialCommentsUl = document.querySelector('.social__comments');
-const socialComments = document.querySelectorAll('.social__comment');
 const buttonCancel = document.querySelector('.big-picture__cancel');
 
-// Функция ниже отображает показ картинки в полном размере в разметке
 
-const getFullSizeImage = () => {
-  for (let j = 0; j < socialComments.length; j++) {
-    socialComments[j].remove();
+const onModalEsc = (event) => {
+  if (event.key === 'Escape'){
+    closeModal();
   }
+
+};
+
+const openModal = () => {
   fullScreenDisplay.classList.remove('hidden');
-  viewPhoto.querySelector('img').src = getDescriptions()[0].url;
-  likesCount.textContent = getDescriptions()[0].likes;
-  commentsCount.textContent = getDescriptions()[0].comments.length;
-  for (let i = 0; i < getDescriptions()[0].comments.length; i++) {
+  commentCounter.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onModalEsc);
+};
+
+function closeModal () {
+  fullScreenDisplay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onModalEsc);
+}
+
+// Функция ниже отображает в разметке показ картинки в полном размере
+
+const getFullSizeImage = (photo) => {
+  socialCommentsUl.innerHTML = null;
+  viewPhoto.querySelector('img').src = photo.url;
+  likesCount.textContent = photo.likes;
+  commentsCount.textContent = photo.comments.length;
+
+  photo.comments.forEach((comment) => {
     const li = document.createElement('li');
     li.classList.add('social__comment');
     const img = document.createElement('img');
     img.classList.add('social__picture');
-    img.src = getDescriptions()[0].comments[i].avatar;
-    img.alt = getDescriptions()[0].comments[i].name;
+    img.src = comment.avatar;
+    img.alt = comment.name;
     img.width = 35;
     img.height = 35;
     li.append(img);
     const p = document.createElement('p');
     p.classList.add('social__text');
-    p.textContent = getDescriptions()[0].comments[i].message;
+    p.textContent = comment.message;
     li.append(p);
     socialCommentsUl.append(li);
-  }
+  });
+
+  openModal();
   buttonCancel.addEventListener('click', (evt) => {
     evt.preventDefault();
-    fullScreenDisplay.classList.add('hidden');
+    closeModal();
   });
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {fullScreenDisplay.classList.add('hidden');}
-  });
-  commentCounter.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
-  body.classList.add('modal-open');
 };
 
 export {getFullSizeImage};
